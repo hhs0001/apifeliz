a2a044f4f98bd66be9b381d8f5317cc106e8b46d
hhs0001 <heitorstein@gmail.com>
2022-09-30 15:42:44 -0300

    primeiro commit

---
+++ AI/AI.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+import tensorflow_datasets as tfds
+import tensorflow as tf
+
+tfds.disable_progress_bar()
+
+dataset, info = tfds.load('imdb_reviews', with_info=True,
+                          as_supervised=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+VOCAB_SIZE = 1000
+encoder = tf.keras.layers.TextVectorization(
+    max_tokens=VOCAB_SIZE)
+encoder.adapt(train_dataset.map(lambda text, label: text))
+
+
+vocab = np.array(encoder.get_vocabulary())
+vocab[:20]
+
+
+model = tf.keras.Sequential([
+    encoder,
+    tf.keras.layers.Embedding(len(encoder.get_vocabulary()), 64, mask_zero=True),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(1)
+])
+
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer=tf.keras.optimizers.Adam(1e-4),
+              metrics=['accuracy'])
+
+history = model.fit(train_dataset, epochs=10,
+                    validation_data=test_dataset,
+                    validation_steps=30)
+
+test_loss, test_acc = model.evaluate(test_dataset)
+
+print('Test Loss:', test_loss)
+print('Test Accuracy:', test_acc)
+
+#save the model
+
+model.save('model.h5')
