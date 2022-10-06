a6027a38393cc04e38d64b6ac4bec68c2b1e3091
hhs0001 <heitorstein@gmail.com>
2022-10-06 20:18:41 -0300

    final update

--- AI/AI.py
+++ AI/AI.py
@@ -1,56 +1,12 @@
-import numpy as np
-import pandas as pd
 import tensorflow as tf
+import pickle
+class AIPredicSentiment:
+    def __init__(self):
+        self.model = tf.keras.models.load_model('./AI/Model3')
+        with open('./AI/tokenizer.pickle', 'rb') as handle:
+            self.tokenizer = pickle.load(handle)
 
-dataset_train = pd.read_csv('twitter_training.csv')
-dataset_val = pd.read_csv('twitter_validation.csv')
-
-dataset_train = dataset_train.drop(columns=['entity', 'Tweet ID'])
-
-dataset_val = dataset_val.drop(columns=['entity', 'Tweet ID'])
-
-x_train = dataset_train["Tweet content"]
-y_train = dataset_train["sentiment"]
-
-x_val = dataset_val["Tweet content"]
-y_val = dataset_val["sentiment"]
-
-BUFFER_SIZE = 10000
-BATCH_SIZE = 64
-
-VOCAB_SIZE = 1000
-encoder = tf.keras.layers.TextVectorization(
-    max_tokens=VOCAB_SIZE)
-encoder.adapt(x_train.values)
-
-
-vocab = np.array(encoder.get_vocabulary())
-vocab[:20]
-
-
-model = tf.keras.Sequential([
-    encoder,
-    tf.keras.layers.Embedding(len(encoder.get_vocabulary()), 64, mask_zero=True),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(1)
-])
-
-model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              optimizer=tf.keras.optimizers.Adam(1e-4),
-              metrics=['accuracy'])
-
-history = model.fit(x_train, epochs=10,
-                    validation_data=y_train,
-                    validation_steps=30)
-
-test_loss, test_acc = model.evaluate(x_val, y_val)
-
-print('Test Loss:', test_loss)
-print('Test Accuracy:', test_acc)
-
-#save the model
-
-model.save('model.h5')
+    def predict(self, text):
+        sequences = self.tokenizer.texts_to_sequences([text])
+        data = tf.keras.preprocessing.sequence.pad_sequences(sequences, maxlen=100)
+        return self.model.predict(data)[0][0]
---
+++ index.py
@@ -0,0 +1,6 @@
+from AI.AI import AIPredicSentiment
+
+# Create an instance of the class
+ai = AIPredicSentiment()
+
+print(ai.predict("I love you"))
